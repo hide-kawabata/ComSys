9e49414c7629ca3ac033d565d9e988b2fe33591e
Hideyuki Kawabata <hide.kawabata@gmail.com>
2024-07-19 12:15:40 +0900

    Add a file

---
+++ OpenMP/mc2.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+
+#define N (1000*1000*1000)
+
+int main() {
+    int i;
+    double pi;
+    long int n = (long)N;
+    long int m = 0;
+    struct timespec ts_start, ts_stop;
+    srand(0);
+    int tid;
+    unsigned int seed;
+/*
+  You can control the number of threads as follows:
+  $ env OMP_NUM_THREADS=4 ./mc
+*/
+// or set the number statically as follows:
+//    omp_set_num_threads(1);
+//    omp_set_num_threads(20);
+//    omp_set_num_threads(100);
+#pragma omp parallel private (tid)
+    {
+        tid = omp_get_thread_num();
+        if (tid == 0) {
+            int nth = omp_get_num_threads();
+            printf("number of threads = %d\n", nth);
+            int nproc = omp_get_num_procs();
+            printf("number of processors = %d\n", nproc);
+        }
+    }
+
+    clock_gettime(CLOCK_REALTIME, &ts_start);
+
+#pragma omp parallel
+    {
+        double x, y;
+//        unsigned int seed = omp_get_thread_num();
+        unsigned int seed = ts_start.tv_nsec;
+        long int m_local = 0;
+#pragma omp for
+        for (i = 0; i < n; i++) {
+            x = ((double)rand_r(&seed)) / ((double)RAND_MAX);
+            y = ((double)rand_r(&seed)) / ((double)RAND_MAX);
+            if (x*x + y*y <= 1.0) {
+                m_local += 1;
+            }
+        }
+#pragma omp atomic
+        m += m_local;
+    }
+    pi = 4.0 * (double)m / (double)n;
+
+    clock_gettime(CLOCK_REALTIME, &ts_stop);
+    double t_diff = (ts_stop.tv_sec * (double)1000000000. + ts_stop.tv_nsec)
+             - (ts_start.tv_sec * (double)1000000000. + ts_start.tv_nsec);
+    printf("n = %ld, m = %ld, pi = %lf\n", n, m, pi);
+    printf("total time:    %10lf\n", t_diff / 1000000000.);
+
+    return 0;
+}
